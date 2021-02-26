bb6b42ff4a3abd4199d2098fc741aca897db20a9
Ian Cossentino <icossentino@gmail.com>
2021-02-26 16:39:09 -0500

    commit tree e2ae277823faf11886fafe132e31b626089b0562

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+
+
+
+
+function superbowlWin(objArray) {
+  let winRecord = objArray.find(function(e) { return e['result'] === "W" })
+  if (!!winRecord) {
+    return winRecord['year']
+  } 
+}
